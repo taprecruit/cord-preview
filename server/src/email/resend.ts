eb543e7707cc2894a42f5ea2f7d15596ab9d5d24
Skyler Sorensen <skyler@datapeople.io>
2026-01-13 15:42:59 -0600

    lazy init resend

--- server/src/email/resend.ts
+++ server/src/email/resend.ts
@@ -1,8 +1,20 @@
 import { Resend } from 'resend';
 import env from 'server/src/config/Env.ts';
 
-if (!env.RESEND_API_KEY) {
-  throw new Error('RESEND_API_KEY is required');
+let resendClient: Resend | null = null;
+
+function getResendClient(): Resend {
+  if (!resendClient) {
+    if (!env.RESEND_API_KEY) {
+      throw new Error('RESEND_API_KEY is required');
+    }
+    resendClient = new Resend(env.RESEND_API_KEY);
+  }
+  return resendClient;
 }
 
-export const resend = new Resend(env.RESEND_API_KEY);
+export const resend = {
+  get emails() {
+    return getResendClient().emails;
+  },
+};
